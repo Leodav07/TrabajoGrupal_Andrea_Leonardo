c2d30bde4bc77f93e75e043cb5d414fc8b5d0465
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-06-13 16:28:20 -0600

    commit

--- tipTapParejas/src/tiptapparejas/logica.java
+++ tipTapParejas/src/tiptapparejas/logica.java
@@ -5,12 +5,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class logica {
+private char tablero[][];
+private char ganador;
+private Usuarios jugador1, jugador2;
+private boolean turnoX; //si es true es de X, si es false es de O
+private boolean finDeJuego;
+public static final char empate = ' ', jX='X', jO='O';
+
+public logica(Usuarios jugador1, Usuarios jugador2){
+    this.jugador1 = jugador1;
+    this.jugador2 = jugador2;
+    turnoX = true;
+    finDeJuego = false;
+    tablero = new char[3][3];
+}
+
+public boolean jugadas(int fila, int columna){
+   if(finDeJuego || fila>2 || fila<0 || columna>2 || columna<0 || tablero[fila][columna]!= empate){
+       return false;
+   }
+   if(turnoX){
+       tablero[fila][columna] = jX;
+   }else{
+       tablero[fila][columna] = jO;
+   }
    
-
-    
-    public logica(String ruta){
-
-    }
-    
-    
+   if(!finDeJuego){
+       turnoX = false;
+   }
+   return true;
+}
 }
